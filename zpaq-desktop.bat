fb23056ba81c938c0588b8f7be0cabf66c084725
Daniel4816 <42698416+Daniel4816@users.noreply.github.com>
2023-04-11 19:40:19 +0200

    Add files via upload

---
+++ zpaq-desktop.bat
@@ -0,0 +1,16 @@
+@echo off
+set threads=%NUMBER_OF_PROCESSORS%
+set zpaq_exe=%appdata%\zpaq\zpaq64.exe
+set input_path=%1
+echo %input_path% | findstr /r "^\" \"$"
+if errorlevel 1 (goto skip) else (set input_path=%input_path:~1,-1%)
+:skip
+For %%A in ("%input_path%") do (
+    Set Folder=%%~dpA
+    Set Name=%%~nxA
+)
+set Name="%Name%"
+cd %Folder%
+echo %Name% | findstr /r .zpaq
+if errorlevel 1 (%zpaq_exe% -t%threads% -m57 a %Name%.zpaq %Name%) else (%zpaq_exe% -t%threads% x %Name%)
+PAUSE
